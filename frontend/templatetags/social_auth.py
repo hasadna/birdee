c729c65afc3075cde027f602157a3c7bf4b2b669
Yehuda Deutsch <yeh@uda.co.il>
2017-10-29 08:41:56 +0200

    Show only social auths with existing keys
    Do something in @18Minuten

--- frontend/templatetags/social_auth.py
+++ frontend/templatetags/social_auth.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -31,13 +32,30 @@ social_buttons = {
 }
 
 
+def has_social_key(network):
+    network_key = f'SOCIAL_AUTH_{network}_key'.replace('-', '_').upper()
+    return hasattr(settings, network_key)
+
+
 @register.assignment_tag()
 def backend_buttons(backends):
     associated = []
     for network in backends['associated']:
+        if not has_social_key(network.provider):
+            continue
         associated.append((network.provider, network, social_buttons.get(network.provider)))
     return {
         'associated': associated,
-        'not_associated': sorted({b: social_buttons[b] for b in backends['not_associated'] if b in social_buttons}.items()),
-        'backends': sorted({b: social_buttons[b] for b in backends['backends'] if b in social_buttons}.items()),
+        'not_associated': sorted(
+            {
+                b: social_buttons[b]
+                for b in backends['not_associated']
+                if b in social_buttons and has_social_key(b)
+            }.items()),
+        'backends': sorted(
+            {
+                b: social_buttons[b]
+                for b in backends['backends']
+                if b in social_buttons and has_social_key(b)
+            }.items()),
     }
